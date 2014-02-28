c5c3bb4a2862f008bee032dbaa541cb00dcba35e
Willem C. van Daalen <wvandaalen@outlook.com>
2014-02-28 00:25:00 -0500

    Finishes contact form setup. Minifies resources and moves scripts to page bottom.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,26 @@ $(document).ready(function() {
 	$('input, textarea').placeholder();
 	$.localScroll({offset: {top: -64}});
 });
+
+$('#contact-form').submit(function() {
+	$.ajax({
+		dataType: 'jsonp',
+		url: 'http://getsimpleform.com/messages/ajax?form_api_token=5268bb70acccc8d2d61fda8949e6d556',
+		data: $('#contact-form').serialize()
+	}).done(function() {
+		// Reset the form
+		$('#contact-form')[0].reset.click();
+
+		// Scroll to success msg
+		$.scrollTo($('#submit-msg'), 500,{offset: {top:-60}});
+
+		// Animate success msg in, then out
+		$('#submit-msg').animate({ opacity: 1 }, 750, 
+			function() {
+				window.setTimeout(function() {
+					$('#submit-msg').animate({ opacity: 0 }, 500);
+				}, 3500);
+			});
+	});
+	return false;		// Prevent normal form submission
+});
